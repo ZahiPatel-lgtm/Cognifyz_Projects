70a6f2d6022800ce4dd28476ea35ae32e60ced90
Dynamic Sales Forecast <zahipatel2007@gmail.com>
2026-05-09 10:50:30 +0530

    Initial commit: code with Program Description

--- str_rev.cpp
+++ str_rev.cpp
@@ -11,3 +11,16 @@ int main()
     }
     cout<<"Reversed string: "<<rev_str<<endl;
 }
+
+// Description
+
+// This program takes a string as input from the user and reverses the string.
+// The reversed string is then displayed on the console.
+// A loop is used to read the string characters from the end to the beginning and store them in a new string.
+
+// Concepts Used
+// Strings
+// Loops
+// User input and output
+// String length function
+// Basic string manipulation
